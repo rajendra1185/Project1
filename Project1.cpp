d25e3faaba8ca587305fa0d3f9a2e4c66ecc2b33
ADHARMAN\RSethi2 <rajendra.sethi2@harman.com>
2020-12-01 13:31:11 +0530

    Add project files.

---
+++ Project1.cpp
@@ -0,0 +1,61 @@
+// Project1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin >> n;
+    vector<int>v;
+    vector<int>::iterator it;
+    int number;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> number;
+        v.push_back(number);
+    }
+    int quer;
+    int qn;
+    cin >> quer;
+    bool check = false;
+    for (int i = 0; i <= quer; i++)
+    {
+        cin >> qn;
+        for (it = v.begin(); it != v.end(); it++)
+        {
+            if (*it == qn)
+            {
+                cout << "Yes" << " " << it - v.begin() + 1 << endl;
+                check = true;
+                break;
+            }
+            check = false;
+        }
+        if (!check)
+        {
+            vector<int>::iterator low;
+            low = lower_bound(v.begin(), v.end(), qn);
+            cout << "No" << " " << *low << endl;
+        }
+    }
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
