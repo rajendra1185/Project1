7fedd00470b96d4884a6afb24f5b34e1ec97bf8b
ADHARMAN\RSethi2 <rajendra.sethi2@harman.com>
2021-03-22 19:25:33 +0530

    Test7-3 to merge

--- Project1.cpp
+++ Project1.cpp
@@ -16,6 +16,7 @@ void print6();
 void print5();
 void print7();
 void print8();
+void print9();
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n;
@@ -86,6 +87,10 @@ void print7()
 void print8()
 {
     cout << "test8" << endl;
+}
+void print9()
+{
+    cout << "test9" << endl;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
