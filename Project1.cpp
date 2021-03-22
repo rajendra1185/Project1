f2b30f3d56e524bb95cc09f4963ded1f99055986
ADHARMAN\RSethi2 <rajendra.sethi2@harman.com>
2021-03-22 11:59:53 +0530

    testing the branch concept

--- Project1.cpp
+++ Project1.cpp
@@ -8,7 +8,7 @@
 #include <algorithm>
 using namespace std;
 
-
+void print();
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     int n;
@@ -48,7 +48,10 @@ int main() {
     return 0;
 }
 
-
+void print()
+{
+    cout << "test1" << endl;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
